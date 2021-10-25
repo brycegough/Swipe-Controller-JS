c9096e9923c64e62677c6d0c59032bf1f63fd699
Bryce Gough <bryce@freeform.com.au>
2021-10-25 14:18:51 +0800

    Update swipe.js

--- swipe.js
+++ swipe.js
@@ -1,5 +1,5 @@
 ($ => {
-    window.SwipeController = function(selector, handlers, options) {
+    window.SwipeController = function(selector, options) {
 
         // Swipe Up / Down / Left / Right
         var initialX = null;
@@ -10,47 +10,41 @@
         options.preventDefault = options.preventDefault !== false;
         this.options = options;
         
-        this.handlers = handlers;
+        this.handlers = {
+            left: this.options.left || false,
+            right: this.options.right || false,
+            up: this.options.up || false,
+            down: this.options.down || false
+        };
         this.selector = selector;
         this.element = $(selector);
-
-
-        if ($(this.element).length > 0) {
-            this.element[0].addEventListener("touchstart", function(e) { this.startTouch(e); }, false);
-            this.element[0].addEventListener("touchmove", function(e) { this.moveTouch(e); }, false);
-        }
 
         /* Functions */
         this.startTouch = (e) => {
             initialX = e.touches[0].clientX;
             initialY = e.touches[0].clientY;
-        };
+        }
 
         this.moveTouch = (e) => {
-            if (initialX === null) {
+            if (initialX === null || initialY === null) {
               return;
             }
 
-            if (initialY === null) {
-              return;
-            }
-
-            var currentX = e.touches[0].clientX;
-            var currentY = e.touches[0].clientY;
-
-            var diffX = initialX - currentX;
-            var diffY = initialY - currentY;
+            var currentX = e.touches[0].clientX,
+                currentY = e.touches[0].clientY,
+                diffX = initialX - currentX,
+                diffY = initialY - currentY;
 
             if (Math.abs(diffX) > Math.abs(diffY)) {
               // sliding horizontally
               if (diffX > 0) {
                 // swiped left
-                if (this.handlers.hasOwnProperty('left') && typeof this.handlers.left === 'function') {
+                if (typeof this.handlers.left === 'function') {
                     this.handlers.left(e);
                 }
               } else {
                 // swiped right
-                if (this.handlers.hasOwnProperty('right') && typeof this.handlers.right === 'function') {
+                if (typeof this.handlers.right === 'function') {
                     this.handlers.right(e);
                 }
               }
@@ -58,12 +52,12 @@
               // sliding vertically
               if (diffY > 0) {
                 // swiped up
-                if (this.handlers.hasOwnProperty('up') && typeof this.handlers.up === 'function') {
+                if (typeof this.handlers.up === 'function') {
                     this.handlers.up(e);
                 }
               } else {
                 // swiped down
-                if (this.handlers.hasOwnProperty('down') && typeof this.handlers.down === 'function') {
+                if (typeof this.handlers.down === 'function') {
                     this.handlers.down(e);
                 }
               }
@@ -75,7 +69,13 @@
             if (this.options.preventDefault) {
                 e.preventDefault();
             }
-        };
+        }
+
+        // Add event listeners
+        if ($(this.element).length > 0) {
+            this.element[0].addEventListener("touchstart", function(e) { this.startTouch(e); }, false);
+            this.element[0].addEventListener("touchmove", function(e) { this.moveTouch(e); }, false);
+        }
 
     };
   })(jQuery);
